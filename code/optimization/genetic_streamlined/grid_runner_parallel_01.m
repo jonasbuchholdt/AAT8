01e0eb2dabc70c829f8c0b4d4b32544417e1ff07
Christoph Kirsch <ckirsc17@student.aau.dk>
2018-04-11 08:39:33 +0200

    computation results from server

--- code/optimization/genetic_streamlined/grid_runner_parallel_01.m
+++ code/optimization/genetic_streamlined/grid_runner_parallel_01.m
@@ -5,12 +5,12 @@ Ly=Lx;
 times=[100 100];
 
 master=struct;
-parpool
+parpool(4)
 spmd
 for k=1:length(Ly)
     for h=1:length(Lx)
         tic;
-        master.og.(strcat('S_',int2str(Lx(h)*100),'_',int2str(Ly(k)*100)))=fixed_gen(Lx(h),Ly(k),50,2);
+        master.og.(strcat('S_',int2str(Lx(h)*100),'_',int2str(Ly(k)*100)))=fixed_gen(Lx(h),Ly(k),400,35);
         times=[times(end-1:end) toc]
         estimated_finish=mean(times)*(length(Ly)*length(Lx)-(h+(k*h-1)));
         display(strcat('Performing Optimizations, time left: ',int2str(floor(estimated_finish/3600)),':',int2str(floor(mod(estimated_finish,3600)/60)),':',int2str(floor(mod(estimated_finish,60)))))
