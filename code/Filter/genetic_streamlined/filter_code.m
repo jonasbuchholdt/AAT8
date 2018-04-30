232628e78eb963df3518df08e572a3113941cfa3
jonas buchholdt <jbuchh13@student.aau.dk>
2018-04-30 16:08:29 +0200

    filter

--- code/Filter/genetic_streamlined/filter_code.m
+++ code/Filter/genetic_streamlined/filter_code.m
@@ -29,9 +29,10 @@ frz = [1:1:460];
 
 %plot(frz,filter_gain)
 figure
-semilogx(-((polyval(p_gain,f_full))+8.5+2),'b--')
+semilogx(-((polyval(p_gain,f_full))+8.5),'b--')
 hold on
-semilogx(f,-(-amplitude1+full_filter_gain(175-60)-1+8.5+2),'o','color','b')
+semilogx(f,-(-amplitude1+full_filter_gain(175-60)-1+8.5),'o','color','b')
+
  ylabel('Gain [dB]')
  xlabel('Frequency [Hz]')
 %yyaxis right
@@ -42,6 +43,9 @@ slope = (full_filter_gain(11)-full_filter_gain(1))/(log10(fr(11))-log10(fr(1)))
 
 % bandwidth = 341 Hz - 128 hz
 
+%for k=1:1:10000
+%h_boost(k) = (1+(((omega_0/Q*s(k))/(s(k)^2+omega_0/Q*s(k)+omega_0^2))*2.3));
+%end
 
 %bandwidth = (341-128*1.1878)*1.3175;
 %omega_0 = 234-0;
@@ -50,17 +54,19 @@ bandwidth = 2*pi*213
 omega_0 = 2*pi*234
 gain = 10^(8.5/20)
 
+%bandwidth = 2*pi*(213+40)
+%omega_0 = 2*pi*234
+%gain = 10^(7.2/20)
+
 Q = omega_0/bandwidth;
 
-omega = [1:1:10000];
+omega = [1:1:100000];
 s = i.*omega;
 
-for k=1:1:10000
-h_boost(k) = (1+(((omega_0/Q*s(k))/(s(k)^2+omega_0/Q*s(k)+omega_0^2))*2.3));
-end
 
-for k=1:1:10000
-h_cut(k) = ((((s(k)^2+omega_0/Q*s(k)+omega_0^2)/(s(k)^2+omega_0/Q*s(k)*(1+gain)+omega_0^2))));
+
+for k=1:1:100000
+h_cut(k) = ((((s(k)^2+omega_0/Q*s(k)+omega_0^2)/(s(k)^2+omega_0/Q*s(k)*(gain)+omega_0^2))));
 end
 
 abs_h = abs(h_cut);
@@ -69,9 +75,9 @@ abs_h = abs(h_cut);
 
 db_abs_h = 20*log10(abs_h);
 
-x = [1:1:10000]/(2*pi);
+x = [1:1:100000]/(2*pi);
 
 semilogx(x,db_abs_h,'color','b')
 
 
-xlim([20 1000])
+xlim([20 20000])
