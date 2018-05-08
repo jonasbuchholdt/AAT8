3549434fa3f17200e27effa6eb4033453d2972d9
jonas buchholdt <jbuchh13@student.aau.dk>
2018-05-08 13:58:19 +0200

    f

--- code/simulation/FDTD_simulation_finish.m
+++ code/simulation/FDTD_simulation_finish.m
@@ -1,9 +1,9 @@
 clear all
 load('impulse_response_3D_40m')
 load('pressureout_03.mat')
-room_x = 2;
-room_y = 2;
-room_z = 2;
+room_x = 30;
+room_y = 30;
+room_z = 30;
 grid_size = 0.05;
 simulation_step = (room_x/grid_size)
 %[impulse_response] = FDTD_impulse_response(room_x,room_y,room_z,grid_size,simulation_step+120);
